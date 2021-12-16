6e072daf4b22f009e12cef6f5039bad57631ed56
lilvina <davinaleo2007@yahoo.com>
2021-12-15 21:41:18 -0800

    refactoring

--- index.js
+++ index.js
@@ -6,36 +6,26 @@ const { chromium } = require("playwright");
   });
 
   // YOUR CODE STARTS
-  try {
     const email = "email@email.com"
     const password = "password"
     const page = await browser.newPage()
     await page.goto("https://www.netflix.com/login")
     await page.fill("input[name='userLoginId']", email)
     await page.fill("input[name='password']", password)
-
+    //await page.click(".btn login-button btn-submit btn-small")
+    //await page.press("Sign In")
     const buttonSignIn = await page.locator("button[type='submit']").first()
     await buttonSignIn.click()
 
-    try {
-      const errorSignIn = await page.waitForSelector(
-        ".ui-message-error, .inputError",
+    const errorSignIn = await page.waitForSelector(
+      ".ui-message-error, .inputError",
         {
           timeout: 10000,
         }
-      )
-      const errMessage = await errorSignIn.innerText()
-    } catch(err) {
-      if(err instanceof errors.TimeoutError) {
-        console.log("There is no error signing in.")
-      } else {
-        console.log(err)
-      }
-    }
-  } catch(err) {
-    console.log(err)
-  } finally {
-    browser.close()
-  }
+    )
+    await errorSignIn.innerText()
+
+    await browser.close()
+
   // YOUR CODE ENDS
 })();
